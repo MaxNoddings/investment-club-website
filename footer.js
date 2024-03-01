fe0f042e5de6a28d9a4268ef53a70dedca68625d
Max Noddings <mnodd4@gmail.com>
2024-02-29 23:36:46 -0500

    created other pages, connected links to each page

---
+++ footer.js
@@ -0,0 +1,12 @@
+// Create the footer content
+const footerContent = `
+  <footer>
+    <div>
+      <p> Copyright <i class="fa-regular fa-copyright"></i> 2024 Saint Micheal's College Student Investment Fund<br><br></p>
+      <p> Website by Max Noddings </p>
+    </div>
+  </footer>
+`;
+
+// Insert the footer content into the body
+document.body.insertAdjacentHTML('beforeend', footerContent);
