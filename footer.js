650bfe0747d89a0cc30af0d0a50a496fb46ee14e
Max Noddings <mnodd4@gmail.com>
2024-03-06 22:45:33 -0500

    working on responsive team page

--- footer.js
+++ footer.js
@@ -3,7 +3,11 @@ const footerContent = `
   <footer>
     <div>
       <p> Copyright <i class="fa-regular fa-copyright"></i> 2024 Saint Micheal's College Student Investment Fund<br><br></p>
-      <p> Website by Max Noddings </p>
+      <div>
+        <a class="plug" href="https://maxnoddings.netlify.app/" target="_blank">
+          <p> Website by Max Noddings </p>
+        </a>
+      </div>
     </div>
   </footer>
 `;
