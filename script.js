41dbc8625334cafa9bc15591468b7846ac4c28fe
Max Noddings <mnodd4@gmail.com>
2024-03-28 23:25:27 -0600

    hamburger menu bug fixes

--- script.js
+++ script.js
@@ -1,34 +1,57 @@
 const body = document.body;
 let lastScroll = 0;
+let sidebarDisplayed = false;
 
 window.addEventListener("scroll", () => {
-	const currentScroll = window.pageYOffset;
-	if (currentScroll <= 0) {
-		body.classList.remove("scroll-up");
-		return;
-	}
+    if (!sidebarDisplayed) {
+        const currentScroll = window.pageYOffset;
+        if (currentScroll <= 0) {
+            body.classList.remove("scroll-up");
+            return;
+        }
 
-	if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
-		body.classList.remove("scroll-up");
-		body.classList.add("scroll-down");
-	} else if (
-		currentScroll < lastScroll &&
-		body.classList.contains("scroll-down")
-	) {
-		body.classList.remove("scroll-down");
-		body.classList.add("scroll-up");
-	}
-	lastScroll = currentScroll;
+        if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+            body.classList.remove("scroll-up");
+            body.classList.add("scroll-down");
+        } else if (
+            currentScroll < lastScroll &&
+            body.classList.contains("scroll-down")
+        ) {
+            body.classList.remove("scroll-down");
+            body.classList.add("scroll-up");
+        }
+        lastScroll = currentScroll;
+    }
 });
 
-function showSidebar() {
-	const sidebar = document.querySelector(".sidebar");
-	sidebar.style.display = "flex";
+// function showSidebar() {
+//     const sidebar = document.querySelector(".sidebar");
+//     sidebar.style.display = "flex";
+//     sidebarDisplayed = true;
+//     // Disable scroll event listener
+//     sidebar.style.overflowY = "hidden";
+// 	// Store the current scroll position
+//     const scrollPosition = window.pageYOffset;
+// 	// Restore the scroll position
+//     window.scrollTo(0, scrollPosition);
+// };
+
+function showSidebar(event) {
+    event.preventDefault(); // Prevent default anchor tag behavior
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "flex";
+    sidebarDisplayed = true;
+    // Disable scroll event listener
+    sidebar.style.overflowY = "hidden";
 };
 
-function hideSidebar() {
-	const sidebar = document.querySelector(".sidebar");
-	sidebar.style.display = "none";
+function hideSidebar(event) {
+	event.preventDefault(); // Prevent default anchor tag behavior
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "none";
+    sidebarDisplayed = false;
+    // Re-enable scroll event listener
+    sidebar.style.overflowY = "auto";
 };
 
 document.addEventListener("DOMContentLoaded", function() {
