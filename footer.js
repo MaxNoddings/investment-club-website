f71fe25981be7bb02074ba29dfb6747c0684b72f
Max Noddings <mnodd4@gmail.com>
2024-04-17 13:34:12 -0600

    added footer logo

--- footer.js
+++ footer.js
@@ -2,7 +2,8 @@
 const footerContent = `
   <footer>
     <div>
-      <p> Copyright <i class="fa-regular fa-copyright"></i> 2024 Saint Michael's College Student Investment Fund<br><br></p>
+    <img class="footer-logo" src="/public/favicon.png" alt="logo-img"/>  
+    <p> Copyright <i class="fa-regular fa-copyright"></i> 2024 Saint Michael's College Student Investment Fund<br><br></p>
       <div>
         <a class="plug" href="https://maxnoddings.netlify.app/" target="_blank">
           <p> Website by Max Noddings </p>
